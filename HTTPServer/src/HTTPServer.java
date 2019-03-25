e4c4cc3cea44cb9049f33f1b01a92f1ac44bd2ff
Ortega Acuna <eortega1793@gmail.com>
2019-03-24 23:06:50 -0600

    refactoring code

--- HTTPServer/src/HTTPServer.java
+++ HTTPServer/src/HTTPServer.java
@@ -19,6 +19,9 @@ import java.util.Map;
 
 public class HTTPServer implements Runnable{
 	private static final File WEB_ROOT = new File(".");
+	private static final String HEAD = "HEAD";
+	private static final String GET = "GET";
+	private static final String POST = "POST";
 	private static final String DEFAULT_FILE = "index.html";
 	private static final String NOT_FOUND = "404.html";
 	private static final String NOT_ACCEPTABLE = "406.html";
@@ -27,15 +30,12 @@ public class HTTPServer implements Runnable{
 	private Socket connection;
 	private Map<String, String> mimeTypes;
 
-
 	public HTTPServer(Socket connection)
 	{
 		this.connection = connection;
 		mimeTypes = new HashMap<String, String>();
 		this.cargar_mimeTypes();
 	}
-
-
 
 	public static void Init() {
 		try
@@ -54,7 +54,7 @@ public class HTTPServer implements Runnable{
 		}
 		catch(IOException e)
 		{
-			System.err.println("Error de conexion en el servidor" + e.getMessage());
+			System.err.println("Connection error" + e.getMessage());
 		}
 	}
 
@@ -64,89 +64,73 @@ public class HTTPServer implements Runnable{
 		BufferedReader input = null;
 		PrintWriter output = null;
 		BufferedOutputStream outputData = null;
-		String requestedFile = null;
-
+		String requestedFileName = null;
+		File requestedFile = null;
 		try
 		{
 			input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			output = new PrintWriter(connection.getOutputStream());
 			outputData = new BufferedOutputStream(connection.getOutputStream());
-
 			String inputData = input.readLine();
 			StringTokenizer parser = new StringTokenizer(inputData);
-			String metodo = parser.nextToken().toUpperCase();
+			String httpMethod = parser.nextToken().toUpperCase();
+			requestedFileName = parser.nextToken().toLowerCase();
 
-			requestedFile = parser.nextToken().toLowerCase();
-
-			if(!metodo.equals("HEAD") && !metodo.equals("GET") && !metodo.equals("POST"))
+			if(requestedFileName.endsWith("/"))
 			{
-				System.out.println("501 Not implemented: " + metodo + " method");
-
-				File archivo = new File(WEB_ROOT, NOT_IMPLEMENTED);
-				int tamano_archivo = (int)archivo.length();
-				String contentMimeType = "text/html";
-				byte[] datos_archivo = leer_datos_archivo(archivo, tamano_archivo);
-
-				System.out.println("HTTP/1.1 501 Not Implemented");
-				System.out.println("Servidor: servidor http");
-				System.out.println("Date: " + new Date());
-				System.out.println("Content-type: " + contentMimeType);
-				System.out.println("Content-length: " + tamano_archivo);
-				System.out.println();//linea en blanco entre el header y el documento
-				System.out.flush();
-
-				//archivo
-				outputData.write(datos_archivo, 0, tamano_archivo);
-				outputData.flush();
+				requestedFileName += DEFAULT_FILE;
 			}
-			else
+
+			requestedFile = new File(WEB_ROOT, requestedFileName);
+			int fileSize = (int) requestedFile.length();
+			String contentMimeType = getContentType(requestedFileName);
+
+			Switch(httpMethod)
 			{
-				//GET, HEAD o POST
-				if(requestedFile.endsWith("/"))
-				{
-					requestedFile += DEFAULT_FILE;
-				}
-
-				File archivo = new File(WEB_ROOT, requestedFile);
-				int tamano_archivo = (int) archivo.length();
-				String contenido = getContentType(requestedFile);
-
-				if(metodo.equals("GET"))
-				{
-					byte[] datos_archivo = leer_datos_archivo(archivo, tamano_archivo);
-
-
+				case HEAD:
+					
+				case GET:
+					byte[] dataFile = readDataFile(requestedFile, fileSize);
 					output.println();
 					output.println("HTTP/1.1 200 OK");
 					output.println("Servidor: servidor http");
 					output.println("Date: " + new Date());
-					output.println("Content-type: " + contenido);
-					output.println("Content-length: " + tamano_archivo);
-					//salida.println("GET");
-					output.println();//linea en blanco entre el header y el documento
+					output.println("Content-type: " + contentMimeType);
+					output.println("Content-length: " + fileSize);
+					output.println();
 					output.flush();
-
-					outputData.write(datos_archivo, 0, tamano_archivo);
-
+					outputData.write(dataFile, 0, fileSize);
 					outputData.flush();
-
-				}
+				case POST:
+					
+				default:
+					System.out.println("501 Not implemented: " + httpMethod + " method");
+					requestedFile = new File(WEB_ROOT, NOT_IMPLEMENTED);
+					int fileSize = (int)requestedFile.length();
+					String contentMimeType = "text/html";
+					byte[] fileData = readDataFile(requestedFile, fileSize);
+					System.out.println("HTTP/1.1 501 Not Implemented");
+					System.out.println("Server: HTTP Server");
+					System.out.println("Date: " + new Date());
+					System.out.println("Content-type: " + contentMimeType);
+					System.out.println("Content-length: " + fileSize);
+					System.out.println();
+					System.out.flush();
+					outputData.write(fileData, 0, fileSize);
+					outputData.flush();
 			}
-
 		}
 		catch(FileNotFoundException fnfe)
 		{
 			try
 			{
-				archivo_no_encontrado(output, outputData, requestedFile);
+				fileNotFound(output, outputData, requestedFileName);
 			}
 			catch (IOException ioe)
 			{
 				System.err.println("Error con archivo no encontrado: " + ioe.getMessage());
 			}
 		}
-
-
 		catch(IOException ioe)
 		{
 			System.err.println("Error del servidor: " + ioe);
@@ -167,7 +151,7 @@ public class HTTPServer implements Runnable{
 		}
 	}
 
-	private byte[] leer_datos_archivo(File archivo, int tamano) throws IOException
+	private byte[] readDataFile(File archivo, int tamano) throws IOException
 	{
 		FileInputStream archivo_entrada = null;
 		byte[] datos_archivo = new byte[tamano];
@@ -243,12 +227,12 @@ public class HTTPServer implements Runnable{
 	}
 
 
-	private void archivo_no_encontrado(PrintWriter salida, OutputStream datos_salida, String archivo_solicitado) throws IOException
+	private void fileNotFound(PrintWriter salida, OutputStream datos_salida, String archivo_solicitado) throws IOException
 	{
 		File archivo = new File(WEB_ROOT, NOT_FOUND);
 		int tamano_archivo = (int)archivo.length();
 		String contenido = "text/html";
-		byte[] datos_archivo = leer_datos_archivo(archivo, tamano_archivo);
+		byte[] datos_archivo = readDataFile(archivo, tamano_archivo);
 
 		salida.println("HTTP/1.1 404 Not found");
 		salida.println("Servidor: servidor http");
