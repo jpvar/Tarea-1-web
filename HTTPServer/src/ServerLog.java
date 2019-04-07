c1210dee228922054980b1cf3d49543fb9a32ff3
Ortega Acuna <eortega1793@gmail.com>
2019-04-07 12:13:43 -0600

    changes on log file

--- HTTPServer/src/ServerLog.java
+++ HTTPServer/src/ServerLog.java
@@ -8,27 +8,25 @@ public class ServerLog {
     private static Logger logger;
     public ServerLog() {
         try {
-            logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+            logger = Logger.getLogger(Thread.currentThread().getStackTrace()[0].getClassName());
             logger.setLevel(Level.INFO);
-            fileTxt = new FileHandler("log/console.log");
+            fileTxt = new FileHandler("log/console.log", true);
             formatterTxt = new SimpleFormatter();
             fileTxt.setFormatter(formatterTxt);
             logger.addHandler(fileTxt);
         }
         catch (IOException e) {
-            logger.warning("Failed to initialize logger handler.");
+            logger.warning("Failed to initialize logger handler. " + e.toString());
         }
     }
 
     public static void log(String method, String server, String referer, String url, String data) {
-        Calendar today = Calendar.getInstance();
-        today.set(Calendar.HOUR_OF_DAY, 0);
         StringBuilder message = new StringBuilder();
-        message.append("[" + today + "]: ");
         message.append("|" + method + "|");
-        message.append("|" + server + "|");
-        message.append("|" + referer + "|");
-        message.append("|" + url + "|");
-        message.append("|" + data + "|");
+        message.append(server + "|");
+        message.append(referer + "|");
+        message.append(url + "|");
+        message.append(data);
+        logger.info(String.valueOf(message));
     }
 }
